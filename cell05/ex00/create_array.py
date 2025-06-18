00d7829f2f6f968804edbe63d160158f21ce6bb3
rsueknok <plamnaka01@gmail.com>
2025-06-18 15:18:49 +0000

    create_array.py

--- cell05/ex00/create_array.py
+++ cell05/ex00/create_array.py
@@ -1,6 +1,3 @@
 array = [2, 8, 9, 48, 8, 22, -12, 2]
 print (array)
  
-
-print("Original array:", array)
-print("New array:", new_array)
--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,4 +1,4 @@
 array = [2, 8, 9, 48, 8, 22, -12, 2]
 new_array = [value +2 for value in array]
-print (array)
-print (new_array)
+print("Original array:", array)
+print("New array:", new_array) 
