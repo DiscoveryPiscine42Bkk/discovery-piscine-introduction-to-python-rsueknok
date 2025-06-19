80141ba5d24b9d8efda367fb328cde0048e91cb9
rsueknok <plamnaka01@gmail.com>
2025-06-19 02:57:45 +0000

    ...

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,8 +1,4 @@
-def main():
-    original = [2, 8, 9, 48, 8, 22, -12, 2]
-    new = [x + 2 for x in original if x > 5]
-
-    print(original, end='')
-    print()
-    print(new)
-main()
+array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = [value + 2 for value in array if value >5]
+print(array)
+print(new_array)
