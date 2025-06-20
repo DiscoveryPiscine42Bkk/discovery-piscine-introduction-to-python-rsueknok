f0b27067136120fd51c198b4a044529d7aef2818
rsueknok <plamnaka01@gmail.com>
2025-06-20 05:28:12 +0000

    scan_it.py

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,20 @@
+import sys
+
+def main():
+
+    if len(sys.argv) != 3:
+        print("none")
+        return
+
+    keyword = sys.argv[1]
+    text = sys.argv[2]
+
+    count = text.count(keyword)
+
+    if count == 0:
+        print("none")
+    else:
+        print(count)
+
+if __name__ == "__main__":
+    main()
